5e40f0b5b156de89b9e9a2a2f0ec9f42c8bcb884
linus <pjlintw@gmail.com>
2021-02-16 23:35:27 +0100

    nn

--- nn.py
+++ nn.py
@@ -24,8 +24,21 @@ class Neuron(Module):
     def __repr__(self):
         return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
+
 class Layer(Module):
-    pass
+    def __init__(self, nin, nout, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [param for n in self.neurons for param in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
 
 class DenseNetwork(Module):
     pass
